ce5df8ca95716e651492195bd91e8f174f0617a9
Arun <arunacharya255@gmail.com>
2022-09-10 08:28:04 +0545

    added favourite table

---
+++ database/factories/FavouriteFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Movie;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Favourite>
+ */
+class FavouriteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => $this->faker->randomElement(User::pluck('id')),
+            'movie_id' => $this->faker->randomElement(Movie::pluck('id'))
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,5 +26,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call(UserSeeder::class);
         $this->call(MovieSeeder::class);
+        $this->call(FavouriteSeeder::class);
     }
 }
